cfb5d92126fef0eb772742f5cc3091617965605f
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2021-12-19 15:05:50 -0800

    set up login and logout in project urls and templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,13 @@
+from os import name
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # Homepage
     path("", include("blog/urls")),
+    path("accounts/login/", views.login, name="login"),
+    path("accounts/logout/", views.logout, name="logout", kwargs={"next_page": "/"}),
 ]
