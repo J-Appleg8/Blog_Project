27135b19e9ad21b790c9d13f0d656d1b2a0315c7
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2021-12-19 13:15:07 -0800

    started models in part 1

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,44 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField
+from django.utils import timezone
+from django.urls import reverse
+
+
+class Post(models.Model):
+    author = models.ForeignKey("auth.User", on_delete=CASCADE)
+    title = models.CharField(max_length=256)
+    text = models.CharField(max_length=256)
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey("blog.POST", on_delete=CASCADE, related_name="comments")
+    author = models.CharField(max_length=256)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_list")
+
+    def __str__(self):
+        return self.text
