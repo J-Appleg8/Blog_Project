ee6769f1b931ced19ebf881d7327ba1cf26e629c
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2021-12-19 14:23:12 -0800

    finished views and urls

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,58 @@
+from typing import List
 from django.shortcuts import render
+from django.utils import timezone
+from django.urls import reverse_lazy
+from blog.forms import PostForm, CommentForm
+from blog.models import Post, Comment
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    TemplateView,
+    DetailView,
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by("-published_date")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    login_url = "/login/"
+    redirect_field_name = "blog/post_detail.html"
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = "/login/"
+    redirect_field_name = "blog/post_detail.html"
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy("post_list")
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = "/login/"
+    redirect_field_name = "blog/post_list.html"
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by("created_date")
